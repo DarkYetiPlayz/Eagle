64eb30106abe697cd1121ac5683e165da000b7a7
DarkYetiPlayz <45898139+DarkYetiPlayz@users.noreply.github.com>
2018-12-15 17:17:26 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.NTIzNjQ0ODA4MjQ2NDYwNDI2.DvcjKw.Kqno08pT5zyl6akip99YwpUDTF8);
